82403630f74b196bee49d5830390506b72f8c763
anil <jadhavanil877@gmail.com>
2025-05-14 12:32:01 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+class GreetRequest(BaseModel):
+    name: str
+    
+    
+@app.get("/")
+def read_root():
+    return {"message": "Hello From FastAPI in Docker!"}
+
+@app.post("/greet/{name}")
+def greet_user(name : str):
+    return {"message": f"Hello {name}!"}
+    
